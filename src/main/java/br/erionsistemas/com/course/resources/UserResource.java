fb7b2f723be022e1ee6a239f4ad8fe507b9234e1
unknown <ddenilsonalves00@gmail.com>
2026-06-22 17:54:40 -0300

    User insert

--- src/main/java/br/erionsistemas/com/course/resources/UserResource.java
+++ src/main/java/br/erionsistemas/com/course/resources/UserResource.java
@@ -4,11 +4,10 @@ import br.erionsistemas.com.course.entities.User;
 import br.erionsistemas.com.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,16 +17,23 @@ public class UserResource {
     @Autowired
     private UserService service;
 
-    @GetMapping
+    @GetMapping //endpoint retornar todos os usuários do banco
     public ResponseEntity<List<User>> findAll(){
         List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/{id}") //endpoint retorna o usuário do banco através do id
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping //endpoint inserir dados
+    public ResponseEntity<User> insert(@RequestBody User obj){//Request Body para dizer que o objetor chega com JSon e desserializado para objto user
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
 }
