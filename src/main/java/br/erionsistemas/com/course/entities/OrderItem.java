1e0d5bdea9319cabf92dac00e5acfeff1b048f04
unknown <ddenilsonalves00@gmail.com>
2026-06-22 17:34:52 -0300

    Subtotal e Total methods

--- src/main/java/br/erionsistemas/com/course/entities/OrderItem.java
+++ src/main/java/br/erionsistemas/com/course/entities/OrderItem.java
@@ -63,6 +63,9 @@ public class OrderItem implements Serializable {
         id.setProduct(product);
     }
 
+    public Double getSubTotal(){
+        return price * quantity;
+    }
 
     @Override
     public boolean equals(Object o) {
